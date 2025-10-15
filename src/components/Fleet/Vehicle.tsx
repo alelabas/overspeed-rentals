2cf8a252f3795bd7a9677b1af2c18cb11ce4f098
alelabas <alelabas208@gmail.com>
2025-10-15 16:43:53 -0300

    prices and contact number updated

--- src/components/Fleet/Vehicle.tsx
+++ src/components/Fleet/Vehicle.tsx
@@ -1,20 +1,19 @@
-import React from "react";
-import { IoMdPeople, IoMdClose } from "react-icons/io";
-import { MdLuggage, MdOutlineInvertColors } from "react-icons/md";
+import React from "react"
+import { IoMdPeople, IoMdClose } from "react-icons/io"
+import { MdLuggage, MdOutlineInvertColors } from "react-icons/md"
 import { IconContext } from "react-icons/lib"
-import { LiaSuitcaseSolid } from "react-icons/lia";
-import { LuFuel } from "react-icons/lu";
-import { Cars } from "../../types/types";
-import { useRef } from "react";
-import { PiApplePodcastsLogo, PiEngine } from "react-icons/pi";
-import { TbAutomaticGearbox } from "react-icons/tb";
-import { Gallery } from "./Gallery";
-import { FormattedMessage } from "react-intl";
+import { LiaSuitcaseSolid } from "react-icons/lia"
+import { LuFuel } from "react-icons/lu"
+import { Cars } from "../../types/types"
+import { useRef } from "react"
+import { PiApplePodcastsLogo, PiEngine } from "react-icons/pi"
+import { TbAutomaticGearbox } from "react-icons/tb"
+import { Gallery } from "./Gallery"
+import { FormattedMessage } from "react-intl"
 
-export default function Vehicle({ vehicle }: {vehicle: Cars}) {
-
+export default function Vehicle({ vehicle }: { vehicle: Cars }) {
 	const refDialog = useRef<HTMLDialogElement>(null)
- 
+
 	const handleClick = () => {
 		refDialog.current?.showModal()
 	}
@@ -26,7 +25,9 @@ export default function Vehicle({ vehicle }: {vehicle: Cars}) {
 				{vehicle.make} {vehicle.model}
 			</h3>
 			<div className="vehicle-specifications">
-				<IconContext.Provider value={{ size: "20px", color: "#ff3c3c" }}>
+				<IconContext.Provider
+					value={{ size: "20px", color: "#ff3c3c" }}
+				>
 					<span className="vehicle-specs">
 						<IoMdPeople />
 						<p>{vehicle.passengers}</p>
@@ -46,52 +47,91 @@ export default function Vehicle({ vehicle }: {vehicle: Cars}) {
 				</IconContext.Provider>
 			</div>
 			<p>
-				<FormattedMessage id="app.fleet.vehicle.from" defaultMessage="From" /> 
+				<FormattedMessage
+					id="app.fleet.vehicle.from"
+					defaultMessage="From"
+				/>
 				<span className="vehicle-price">
 					${vehicle.price}/
-					<FormattedMessage id="app.fleet.vehicle.day" defaultMessage="day" />
+					<FormattedMessage
+						id="app.fleet.vehicle.day"
+						defaultMessage="day"
+					/>
 				</span>{" "}
 				- <span className="discount-price">${vehicle.price + 40}</span>
 			</p>
 			<div className="button-view-more">
 				<button onClick={handleClick}>
-					<FormattedMessage id="app.fleet.button" defaultMessage="View More" />
+					<FormattedMessage
+						id="app.fleet.button"
+						defaultMessage="View More"
+					/>
 				</button>
 			</div>
 
 			<dialog ref={refDialog} className="dialog-modal" closedby="any">
 				<div className="dialog-content">
 					<div className="close-menu">
-						<form method="dialog"><button className="close-button"><IoMdClose color={"black"} size={'25px'}/></button></form>
+						<form method="dialog">
+							<button className="close-button">
+								<IoMdClose color={"black"} size={"25px"} />
+							</button>
+						</form>
 					</div>
-					
+
 					<div className="main-content">
 						<div className="gallery-container">
-							<Gallery vehicleImages={vehicle.images} modalRef={refDialog}/>
+							<Gallery
+								vehicleImages={vehicle.images}
+								modalRef={refDialog}
+							/>
 						</div>
 						<div className="data-vehicle-wrapper">
-							<h3>{vehicle.make} {vehicle.model} {vehicle.year}</h3>
+							<h3>
+								{vehicle.make} {vehicle.model} {vehicle.year}
+							</h3>
 							<h4>
-								<IoMdPeople /> 	
-								<FormattedMessage id="app.fleet.vehicle.passengers" defaultMessage="Passengers"/> : {vehicle.passengers}
+								<IoMdPeople />
+								<FormattedMessage
+									id="app.fleet.vehicle.passengers"
+									defaultMessage="Passengers"
+								/>{" "}
+								: {vehicle.passengers}
 							</h4>
-							<h4><PiApplePodcastsLogo/> Apple Car Play / Android Auto</h4>
 							<h4>
-								<PiEngine/>
-								<FormattedMessage id="app.fleet.vehicle.engine" defaultMessage="Engine"/>: {vehicle.engine}
+								<PiApplePodcastsLogo /> Apple Car Play / Android
+								Auto
 							</h4>
 							<h4>
-								<TbAutomaticGearbox/>
-								<FormattedMessage id="app.fleet.vehicle.transmission" defaultMessage="Transmission"/>
+								<PiEngine />
+								<FormattedMessage
+									id="app.fleet.vehicle.engine"
+									defaultMessage="Engine"
+								/>
+								: {vehicle.engine}
 							</h4>
-							<a href="https://wa.me/+541164026530?text=Hola%20quiero%20mas%20informacion%20sobre%20alquilar%20un%20auto%20en%20Miami" target="_blank" rel="noreferrer" className="modal-button">
-								<FormattedMessage id="app.fleet.vehicle.contact" defaultMessage="Contact Us!" />
+							<h4>
+								<TbAutomaticGearbox />
+								<FormattedMessage
+									id="app.fleet.vehicle.transmission"
+									defaultMessage="Transmission"
+								/>
+							</h4>
+							<a
+								href="https://wa.me/+17868267016"
+								target="_blank"
+								rel="noreferrer"
+								className="modal-button"
+							>
+								<FormattedMessage
+									id="app.fleet.vehicle.contact"
+									defaultMessage="Contact Us!"
+								/>
 							</a>
 						</div>
 					</div>
-				</div>	
+				</div>
 			</dialog>
-
 		</div>
 	)
 }
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,19 +1,30 @@
-import React from "react";
-import "./Hero.css"
-import { FormattedMessage } from "react-intl";
+import React from "react"
+import "Hero.css"
+import { FormattedMessage } from "react-intl"
 
-export function Hero () {
-    return (
-        <section className="hero-section">
-            <div className="hero-contact">
-                <h1>Overspeed Rentals</h1>
-                <p>
-                    <FormattedMessage id="app.hero.paragraph" defaultMessage="Drive Miami Your Way"/>
-                </p>
-                <a href="https://wa.me/+541164026530?text=Hola%20quiero%20mas%20informacion%20sobre%20alquilar%20un%20auto%20en%20Miami" target="_blank" rel="noreferrer" className="modal-button hero-button">
-                    <FormattedMessage id="app.hero.button" defaultMessage="Contact Us"/>
-                </a>
-            </div>
-        </section>
-    )
+export function Hero() {
+	return (
+		<section className="hero-section">
+			<div className="hero-contact">
+				<h1>Overspeed Rentals</h1>
+				<p>
+					<FormattedMessage
+						id="app.hero.paragraph"
+						defaultMessage="Drive Miami Your Way"
+					/>
+				</p>
+				<a
+					href="https://wa.me/+17868267016"
+					target="_blank"
+					rel="noreferrer"
+					className="modal-button hero-button"
+				>
+					<FormattedMessage
+						id="app.hero.button"
+						defaultMessage="Contact Us"
+					/>
+				</a>
+			</div>
+		</section>
+	)
 }
